4f77e57d202e82bd4cae1c074aec3f48ea798cd4
Timothy Yap <tim@timaaronyap.com>
2020-07-07 20:27:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board(board = ["X", "X", "X", " ", "X", " ", " ", " ", " "])
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
